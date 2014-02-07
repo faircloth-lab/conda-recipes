f81df0bfe9701203c1217705e0d5ce56eff994c0
Brant Faircloth <brant@faircloth-lab.org>
2014-02-06 16:15:11 -0800

    make abyss makefile kmer 96, like 96

--- abyss/build.sh
+++ abyss/build.sh
@@ -3,6 +3,6 @@
 # go to source location
 cd $SRC_DIR
 
-./configure --prefix=$PREFIX --with-boost=$PREFIX/include
+./configure --prefix=$PREFIX --with-boost=$PREFIX/include --enable-maxk=96
 make AM_CXXFLAGS=-Wall
 make install
