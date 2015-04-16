194a8db53ddbe7f6b4ee5bdf83a81c05415638b4
Brant Faircloth <brant@faircloth-lab.org>
2015-04-16 16:45:38 -0500

    linux and osx require different sed commands

--- abyss/post-link.sh
+++ abyss/post-link.sh
@@ -1,1 +1,7 @@
-sed -i'' 's|#!/usr/bin/make -rRf|#!'$PREFIX'/bin/condaflmake -rRf|' $PREFIX/bin/abyss-pe
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+    sed -i'' 's|#!/usr/bin/make -rRf|#!'$PREFIX'/bin/condaflmake -rRf|' $PREFIX/bin/abyss-pe
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    sed -i '' 's|#!/usr/bin/make -rRf|#!'$PREFIX'/bin/condaflmake -rRf|' $PREFIX/bin/abyss-pe
+fi
